a2ecf50327c3b7f94dc5d7e58e031d87fbcf4ec0
eburdon <eburdon@uvic.ca>
2015-02-22 15:01:16 -0800

    Continued edits of M1 Readme

--- M1_ComplexityLanguage/1_ActualSource/downloadSizeParser.py
+++ M1_ComplexityLanguage/1_ActualSource/downloadSizeParser.py
@@ -1,5 +1,15 @@
 #/usr/bin/Python
-# Python download size parser
+
+## Python download size parser
+# This script was my first attempt at trying to get all Python download version names, urls, and download sizes.
+# Not only was Selenium difficult to work with, and slow to execute (had to physically open firefox window), I realized
+# it wouldn't be able to handle the different formatting of nearly every Python download page. Each page was formatted slightly
+# different than the others - some were rich webpages with tables, others were simple and only had part of the information, and
+# others were basically text files. As such, I switched to Import.io to gather information for this part.
+
+## USEFUL OUTPUT INFORMATION
+# Number of Python versions available for download
+# Full URL of every Python download link
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -13,13 +23,15 @@ DRIVER = None
 #################################################
 
 def parse_raw_text(DRIVER):
-    print "RAW TEXT FILE, YO"
+    # print "RAW TEXT FILE, YO"
     ## merp = DRIVER.getPageSource().contains("text to search");
+    pass
     
 def parse_web_page(DRIVER):
-    print "FANCY WEB PAGE, YO"
+    # print "FANCY WEB PAGE, YO"
     ## merp = DRIVER.find_element_by_xpath("//div[contains(., 'Release date:')]")
     # print merp.text
+    pass
 
 def main():
     global DRIVER
@@ -27,7 +39,6 @@ def main():
     # Open firefox
     DRIVER = webdriver.Firefox()
     
-    # Open close
     DRIVER.get("https://www.python.org/downloads/")
     assert 'Python.org' in DRIVER.title
     
@@ -35,7 +46,7 @@ def main():
     # listDownloads = DRIVER.find_elements(By.LINK_TEXT,"Release Notes")
     listDownloads = DRIVER.find_elements(By.LINK_TEXT,"Download")
     linksCount = len(listDownloads)
-    print linksCount
+    print "\n Number of Python version links: ", linksCount, "\n"
     
     for item in listDownloads:
         # Get actual url links for each item found in above list
